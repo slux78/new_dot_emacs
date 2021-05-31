9e21fb7b280f2387f6426de981a2d7a629be7618
slux78 <slux78@gmail.com>
2021-06-01 02:04:36 +0900

    Update init.el

--- init.el
+++ init.el
@@ -11,13 +11,12 @@
 ;; Set up the visible bell
 ;;(setq visible-bell t)
 
-;; Set the font face based on platform
 (pcase system-type
   ((or 'gnu/linux 'windows-nt 'cygwin)
    (set-face-attribute 'default nil
                        :font "JetBrains Mono"
                        :weight 'light))
-  ('darwin (set-face-attribute 'default nil :font "Fira Mono" :height 170)))
+  ('darwin (set-face-attribute 'default nil :font "Source Code Pro" :height 170)))
 
 ;; Set the fixed pitch face
 (set-face-attribute 'fixed-pitch nil
@@ -31,14 +30,8 @@
 ;;                    :font "Iosevka Aile"
                     :weight 'light)
 
-;;(load-theme 'tango-dark)
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'vscode-dark-plus t)
-;;(use-package vscode-dark-plus-theme
-;;  :ensure t
-;;  :config
-;;  (load-theme 'vscode-dark-plus t))
 
 ;; Make ESC quit prompt
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -46,7 +39,7 @@
 ;; Initialize package sources
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
@@ -96,6 +89,28 @@
   (ivy-mode 1)
 )
 
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+(use-package wgrep)
+
+(use-package ivy-posframe
+  :disabled
+  :custom
+  (ivy-posframe-width      115)
+  (ivy-posframe-min-width  115)
+  (ivy-posframe-height     10)
+  (ivy-posframe-min-height 10)
+  :config
+  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+  (setq ivy-posframe-parameters '((parent-frame . nil)
+				  (left-fringe . 8)
+				  (right-fringe . 8)))
+  (ivy-posframe-mode 1))
+
 (use-package all-the-icons)
 
 (use-package counsel
@@ -111,6 +126,17 @@
   (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
   :config
   (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package prescient
+  :after counsel
+  :config
+  (prescient-persist-mode 1))
+
+(use-package ivy-prescient
+  :after prescient
+  :config
+  (ivy-prescient-mode 1))
+
 
 ;; You must run (all-the-icons-install-fonts) one time after
 ;; installing this package!
@@ -138,7 +164,7 @@
 
 (doom-modeline-mode)
 
-;;(use-package doom-themes)
+(use-package doom-themes)
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -186,12 +212,6 @@
 
 (use-package undo-tree)
 
-;(defun rune/leader-keys ()
-;  )
-;;  (rune/leader-keys
-;;   "t" '(:ignore t :which-key "toggles")
-;;   "tt" '(counsel-load-theme :which-key "choose theme")))
-
 (use-package general
   :config
   (general-create-definer rune/leader-keys
@@ -236,8 +256,439 @@
   ("k" text-scale-decrease "out")
   ("f" nil "finished" :exit t))
 
+(use-package savehist
+    :config
+    (setq history-length 25)
+    (savehist-mode 1))
+
+;(use-package orderless
+;  :init
+;  (setq completion-styles '(orderless)
+;        completion-category-defaults nil
+;        completion-category-overrides '((file (styles . (partial-completion))))))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(substring orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles . (partial-completion))))))
+
+(defun dw/minibuffer-backward-kill (arg)
+  "When minibuffer is completing a file name delete up to parent
+  folder, otherwise delete a word"
+  (interactive "p")
+  (if minibuffer-completing-file-name
+      ;; Borrowed from https://github.com/raxod502/selectrum/issues/498#issuecomment-803283608
+      (if (string-match-p "/." (minibuffer-contents))
+          (zap-up-to-char (- arg) ?/)
+        (delete-minibuffer-contents))
+    (backward-kill-word arg)))
+
+(use-package vertico
+  :bind (:map vertico-map
+              ("C-j" . vertico-next)
+              ("C-k" . vertico-previous)
+              ("C-f" . vertico-exit)
+              :map minibuffer-local-map
+              ("M-h" . dw/minibuffer-backward-kill))
+  :custom
+  (vertico-cycle t)
+  :custom-face
+  (vertico-current ((t (:background "#3a3f5a"))))
+  :init
+  (vertico-mode))
+
+(use-package corfu
+  :bind (:map corfu-map
+              ("C-j" . corfu-next)
+              ("C-k" . corfu-previous)
+              ("C-f" . corfu-insert))
+  :custom
+  (corfu-cycle t)
+  :config
+  (corfu-global-mode))
+
+(use-package avy
+  :commands (avy-goto-char avy-goto-word-0 avy-goto-line))
+
+(use-package default-text-scale
+  :defer 1
+  :config
+  (default-text-scale-mode))
+
+(use-package ace-window
+  :bind (("M-o" . ace-window))
+  :custom
+  (aw-scope 'frame)
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-minibuffer-flag t)
+  :config
+  (ace-window-display-mode 1))
+
+(use-package markdown-mode
+    :mode "\\.md\\'"
+    :config
+    (setq markdown-command "marked"))
+
+(use-package yasnippet
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all))
+
+(use-package ivy-yasnippet)
+(use-package yasnippet-snippets)
+
+(use-package know-your-http-well
+  :defer t)
+
+(use-package posframe)
+
+(use-package command-log-mode
+  :after posframe)
+
+(setq dw/command-window-frame nil)
+
+(defun dw/toggle-command-window ()
+  (interactive)
+  (if dw/command-window-frame
+      (progn
+        (posframe-delete-frame clm/command-log-buffer)
+        (setq dw/command-window-frame nil))
+    (progn
+      (global-command-log-mode t)
+      (with-current-buffer
+          (setq clm/command-log-buffer
+                (get-buffer-create " *command-log*"))
+        (text-scale-set -1))
+      (setq dw/command-window-frame
+            (posframe-show
+             clm/command-log-buffer
+             :position `(,(- (x-display-pixel-width) 650) . 50)
+             :width 35
+             :height 5
+             :min-width 35
+             :min-height 5
+             :internal-border-width 2
+             :internal-border-color "#c792ea"
+             :override-parameters '((parent-frame . nil)))))))
+
+(use-package keycast
+  :config
+  ;; This works with doom-modeline, inspired by this comment:
+  ;; https://github.com/tarsius/keycast/issues/7#issuecomment-627604064
+  (define-minor-mode keycast-mode
+    "Show current command and its key binding in the mode line."
+    :global t
+    (if keycast-mode
+        (add-hook 'pre-command-hook 'keycast-mode-line-update t)
+      (remove-hook 'pre-command-hook 'keycast-mode-line-update)))
+
+  (add-to-list 'global-mode-string '("" mode-line-keycast " ")))
+
+
+(rune/leader-keys
+ "tc" 'dw/toggle-command-window)
+
+
+(rune/leader-keys
+ "j"   '(:ignore t :which-key "jump")
+ "jj"  '(avy-goto-char :which-key "jump to char")
+ "jw"  '(avy-goto-word-0 :which-key "jump to word")
+ "jl"  '(avy-goto-line :which-key "jump to line"))
+
 (rune/leader-keys
   "tf" '(hydra-text-scale/body :which-key "scale text"))
+
+(rune/leader-keys
+  "r"   '(ivy-resume :which-key "ivy resume")
+  "f"   '(:ignore t :which-key "files")
+  "ff"  '(counsel-find-file :which-key "open file")
+  "C-f" 'counsel-find-file
+  "fr"  '(counsel-recentf :which-key "recent files")
+  "fR"  '(revert-buffer :which-key "revert file")
+  "fj"  '(counsel-file-jump :which-key "jump to file"))
+
+(rune/leader-keys
+  "b"   '(:ignore t :which-key "buffer")
+  "bb" '(counsel-switch-buffer :which-key "switch buffer"))
+
+;;(defun rune/switch-project-action ()
+;;  "Switch to a workspace with the project name and start `magit-status'."
+;;  ;; TODO: Switch to EXWM workspace 1?
+;;  (persp-switch (projectile-project-name))
+;;  (magit-status))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :demand t
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :after projectile
+  :bind (("C-M-p" . counsel-projectile-find-file))
+  :config
+  (counsel-projectile-mode))
+
+;;(rune/leader-key-def
+;; "pf"  'counsel-projectile-find-file
+;; "ps"  'counsel-projectile-switch-project
+;; "pF"  'counsel-projectile-rg
+;; ;; "pF"  'consult-ripgrep
+;; "pp"  'counsel-projectile
+;; "pc"  'projectile-compile-project
+;; "pd"  'projectile-dired)
+
+(use-package magit
+    :commands (magit-status magit-get-current-branch)
+    :custom
+    (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;;(use-package evil-magit
+;;  :after magit)
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+;  (diminish org-indent-mode))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t
+          org-src-fontify-natively t
+          org-fontify-quote-and-verse-blocks t
+          org-src-tab-acts-natively t
+          org-edit-src-content-indentation 2
+          org-hide-block-startup nil
+          org-src-preserve-indentation nil
+          org-startup-folded 'content
+          org-cycle-separator-lines 2))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(font-lock-add-keywords 'org-mode
+			'(("^ *\\([-]\\) "
+			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;(dolist (face '((org-level-1 . 1.2)
+;                  (org-level-2 . 1.1)
+;                  (org-level-3 . 1.05)
+;                  (org-level-4 . 1.0)
+;                  (org-level-5 . 1.1)
+;                  (org-level-6 . 1.1)
+;                  (org-level-7 . 1.1)
+;                  (org-level-8 . 1.1)))
+;    (set-face-attribute (car face) nil :font "Cantarell" :weight 'medium :height (cdr face)))
+
+;; Make sure org-indent face is available
+(require 'org-indent)
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 110
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :defer t
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+(use-package eterm-256color
+  :hook (term-mode . eterm-256color-mode))
+
+;;(use-package fish-completion
+;;  :hook (eshell-mode . fish-completion-mode))
+
+(use-package eshell-syntax-highlighting
+  :after esh-mode
+  :config
+  (eshell-syntax-highlighting-global-mode +1))
+
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode)
+  :config
+  (setq esh-autosuggest-delay 0.5)
+  (set-face-foreground 'company-preview-common "#4b5668")
+  (set-face-background 'company-preview nil))
+
+(use-package eshell-toggle
+  :bind ("C-M-'" . eshell-toggle)
+  :custom
+  (eshell-toggle-size-fraction 3)
+  (eshell-toggle-use-projectile-root t)
+  (eshell-toggle-run-command nil))
+
+(defun efs/configure-eshell ()
+  ;; save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  ;; truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; Bind some useful keys for evil-mode
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
+
+  (setq eshell-history-size 10000
+	eshell-buffer-maximum-lines 10000
+	eshell-hist-ignoredups t
+	eshell-scroll-to-bottom-on-input t))
+
+
+(use-package eshell-git-prompt)
+(use-package eshell
+  :hook (eshell-first-time-mode . efs/configure-eshell)
+  :config
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)
+    (setq eshell-visual-commands '("htop" "zsh" "fish" "vim")))
+
+  (eshell-git-prompt-use-theme 'powerline))
+
+(use-package vterm
+  :commands vterm
+  :config
+)
+
+(use-package all-the-icons-dired)
+
+(use-package dired
+  :ensure nil
+  :defer 1
+  :commands (dired dired-jump)
+  :config
+  (setq dired-listing-switches "-agho --group-directories-first"
+	dired-omit-files "^\\.[^.].*"
+	dired-omit-verbose nil
+	dired-hide-details-hide-symlink-targets nil
+	delete-by-moving-to-trash t)
+
+  (autoload 'dired-omit-mode "dired-x")
+
+  (add-hook 'dired-load-hook
+	    (lambda ()
+	      (interactive)
+	      (dired-collapse)))
+  (add-hook 'dired-mode-hook
+	    (lambda ()
+	      (interactive)
+	      (dired-omit-mode 1)
+	      (dired-hide-details-mode 1)
+	      (all-the-icons-dired-mode 1)
+	      (hl-line-mode 1)))
+
+  (use-package dired-rainbow
+    :defer 2
+    :config
+    (dired-rainbow-define-chmod directory "#6cb2eb" "d.*")
+    (dired-rainbow-define html "#eb5286" ("css" "less" "sass" "scss" "htm" "html" "jhtm" "mht" "eml" "mustache" "xhtml"))
+    (dired-rainbow-define xml "#f2d024" ("xml" "xsd" "xsl" "xslt" "wsdl" "bib" "json" "msg" "pgn" "rss" "yaml" "yml" "rdata"))
+    (dired-rainbow-define document "#9561e2" ("docm" "doc" "docx" "odb" "odt" "pdb" "pdf" "ps" "rtf" "djvu" "epub" "odp" "ppt" "pptx"))
+    (dired-rainbow-define markdown "#ffed4a" ("org" "etx" "info" "markdown" "md" "mkd" "nfo" "pod" "rst" "tex" "textfile" "txt"))
+    (dired-rainbow-define database "#6574cd" ("xlsx" "xls" "csv" "accdb" "db" "mdb" "sqlite" "nc"))
+    (dired-rainbow-define media "#de751f" ("mp3" "mp4" "mkv" "MP3" "MP4" "avi" "mpeg" "mpg" "flv" "ogg" "mov" "mid" "midi" "wav" "aiff" "flac"))
+    (dired-rainbow-define image "#f66d9b" ("tiff" "tif" "cdr" "gif" "ico" "jpeg" "jpg" "png" "psd" "eps" "svg"))
+    (dired-rainbow-define log "#c17d11" ("log"))
+    (dired-rainbow-define shell "#f6993f" ("awk" "bash" "bat" "sed" "sh" "zsh" "vim"))
+    (dired-rainbow-define interpreted "#38c172" ("py" "ipynb" "rb" "pl" "t" "msql" "mysql" "pgsql" "sql" "r" "clj" "cljs" "scala" "js"))
+    (dired-rainbow-define compiled "#4dc0b5" ("asm" "cl" "lisp" "el" "c" "h" "c++" "h++" "hpp" "hxx" "m" "cc" "cs" "cp" "cpp" "go" "f" "for" "ftn" "f90" "f95" "f03" "f08" "s" "rs" "hi" "hs" "pyc" ".java"))
+    (dired-rainbow-define executable "#8cc4ff" ("exe" "msi"))
+    (dired-rainbow-define compressed "#51d88a" ("7z" "zip" "bz2" "tgz" "txz" "gz" "xz" "z" "Z" "jar" "war" "ear" "rar" "sar" "xpi" "apk" "xz" "tar"))
+    (dired-rainbow-define packaged "#faad63" ("deb" "rpm" "apk" "jad" "jar" "cab" "pak" "pk3" "vdf" "vpk" "bsp"))
+    (dired-rainbow-define encrypted "#ffed4a" ("gpg" "pgp" "asc" "bfe" "enc" "signature" "sig" "p12" "pem"))
+    (dired-rainbow-define fonts "#6cb2eb" ("afm" "fon" "fnt" "pfb" "pfm" "ttf" "otf"))
+    (dired-rainbow-define partition "#e3342f" ("dmg" "iso" "bin" "nrg" "qcow" "toast" "vcd" "vmdk" "bak"))
+    (dired-rainbow-define vc "#0074d9" ("git" "gitignore" "gitattributes" "gitmodules"))
+    (dired-rainbow-define-chmod executable-unix "#38c172" "-.*x.*"))
+
+  (use-package dired-single
+    :defer t)
+
+  (use-package dired-ranger
+    :defer t)
+
+  (use-package dired-collapse
+    :defer t)
+  
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-single-up-directory
+    "H" 'dired-omit-mode
+    "l" 'dired-single-buffer
+    "y" 'dired-ranger-copy
+    "X" 'dired-ranger-move
+    "p" 'dired-ranger-paste))
+
+
+(use-package emojify
+  :hook (erc-mode . emojify-mode)
+  :commands emojify-mode)
+
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+(setq-default tab-width 4)
+(setq-default evil-shift-width tab-width)
+
+(setq-default indent-tabs-mode nil)
+
+(use-package evil-nerd-commenter
+  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+(use-package origami
+  :hook (yaml-mode . origami-mode))
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (global-company-mode t))
+
+(use-package company-irony
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
+(use-package irony
+  :ensure t
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -248,11 +699,12 @@
    [default default default italic underline success warning error])
  '(global-command-log-mode t)
  '(package-selected-packages
-   '(hydra evil-collection evil general vscode-dark-plus-theme doom-themes helpful minions ivy-rich which-key rainbow-delimiters doom-modeline ivy command-log-mode use-package)))
+   '(company-irony auto-complete yasnippet-snippets ivy-yasnippet keycast know-your-http-well yasnippet markdown-mode ace-window default-text-scale avy corfu vertico orderless ivy-prescient prescient ivy-posframe wgrep flx origami evil-nerd-commenter diminish emojify eshell-toggle esh-autosuggest eshell-syntax-highlighting fish-completion eterm-256color vterm visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection evil general vscode-dark-plus-theme doom-themes helpful minions ivy-rich which-key rainbow-delimiters doom-modeline ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:height 0.85))))
- '(mode-line-inactive ((t (:height 0.85)))))
+ '(mode-line-inactive ((t (:height 0.85))))
+ '(vertico-current ((t (:background "#3a3f5a")))))
